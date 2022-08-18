4a40dd550189b13d0e01fdcb53ab7580962574eb
Régis Teixeira <regismarcondes2016@gmail.com>
2022-08-18 09:28:21 -0300

    Ajustando resultados para 2 casas decimais

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function porcentagem_1()
     var porcentagem = document.getElementById("porcentagem").value;
 
     var num = document.getElementById("num").value;
-    document.getElementById("valor1").value = (num / 100) * porcentagem;
+    document.getElementById("valor1").value = ((num / 100) * porcentagem).toFixed(2);
 }
 
 function porcentagem_2()
@@ -11,5 +11,5 @@ function porcentagem_2()
     var num1 = document.getElementById("num1").value;
 
     var num2 = document.getElementById("num2").value;
-    document.getElementById("valor2").value = (num1 * 100) / num2 + "%";
+    document.getElementById("valor2").value = ((num1 * 100) / num2 ).toFixed(2) + "%";
 }
